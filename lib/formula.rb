d18ad2c2bd6dc9b11cc48614f6bfa41ad51962f6
Kevin Sylvestre <kevin@ksylvest.com>
2010-11-08 14:39:58 -0500

    integrated railties and added custom form and field generators

--- lib/formula.rb
+++ lib/formula.rb
@@ -4,9 +4,35 @@ module Formula
   
   class FormulaFormBuilder < ActionView::Helpers::FormBuilder
   
-    def input(method, options = {})
+    def input(method, options = {}, &block)
+    end
+    
+    def association(method, options = {}, &block)
+    end
+    
+    def formula_fields_for(record_or_name_or_array, *args, &block)
+      options = args.extract_options!
+      options[:builder] ||= self.class
+      fields_for(record_or_name_or_array, *(args << options), &block)
     end
   
+  end
+  
+  module FormulaFormHelper
+    @@builder = ::Formula::FormulaFormBuilder
+    
+    def formula_form_for(record_or_name_or_array, *args, &proc)
+       options = args.extract_options!
+       options[:builder] ||= @@builder
+       form_for(record_or_name_or_array, *(args << options), &proc)
+    end
+     
+    def formula_fields_for(record_or_name_or_array, *args, &block)
+      options = args.extract_options!
+      options[:builder] ||= @@builder
+      fields_for(record_or_name_or_array, *(args << options), &block)
+    end
+
   end
   
 end
--- lib/formula/railtie.rb
+++ lib/formula/railtie.rb
@@ -4,6 +4,7 @@ require 'rails'
 module Formula
   class Railtie < Rails::Railtie
     initializer 'formula.initialize' do
+      ActionView::Base.send :include, Formula::FormulaFormHelper
     end
   end
 end
