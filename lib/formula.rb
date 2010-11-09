a4247bf7e7da96d078a89b25b41be144532e38cc
Kevin Sylvestre <kevin@ksylvest.com>
2010-11-09 15:24:52 -0500

    updated examples to show usage of class and added hints and errors support as well as container and constants

--- lib/formula.rb
+++ lib/formula.rb
@@ -2,9 +2,59 @@ module Formula
   
   require 'formula/railtie' if defined?(Rails)
   
+  mattr_accessor :container_tag
+  @@container_tag = :div
+  
+  mattr_accessor :hints_tag
+  @@hints_tag = :div
+  
+  mattr_accessor :errors_tag
+  @@errors_tag = :div
+  
+  mattr_accessor :default_input_size
+  @@default_input_size = 50
+  
+  mattr_accessor :default_textarea_cols
+  @@default_textarea_cols = 50
+  
+  mattr_accessor :default_textarea_rows
+  @@default_textarea_rows = 3
+  
   class FormulaFormBuilder < ActionView::Helpers::FormBuilder
+    
+    def as(method, options = {})
+      type = @object.column_for_attribute(method)
+      
+      case type
+        when :string then
+          return :url       if method.to_s =~ /url/
+          return :email     if method.to_s =~ /email/
+          return :phone     if method.to_s =~ /phone/
+          return :password  if method.to_s =~ /password/
+        when :integer then
+          return :number
+        when :float   then
+          return :number
+        when :decimal then
+          return :number
+      end
+      
+      return :text
+    end
   
     def input(method, options = {}, &block)
+      options[:as] ||= as(method, options)
+      options[:errors] ||= @object.errors[method]
+      
+      components = []
+      
+      components << label(:method, options[:label])
+      components << @template.content_tag(::Formula.hints_tag, options[:hint], :class => 'hints') if options[:hints]
+      components << @template.content_tag(::Formula.errors_tag, options[:errors], :class => 'errors') if options[:errors]
+      
+      @template.content_tag(::Formula.container_tag, :class => options[:class]) do
+        components.join
+      end
     end
     
     def association(method, options = {}, &block)
@@ -32,7 +82,9 @@ module Formula
       options[:builder] ||= @@builder
       fields_for(record_or_name_or_array, *(args << options), &block)
     end
-
+    
+    alias :formula_for :formula_form_for
+    alias :fieldula_for :formula_fields_for
   end
   
 end
