fe9897d3c4bdbf400cc9badf97d37874e72ffaff
Kevin Sylvestre <kevin@ksylvest.com>
2011-10-22 22:20:05 -0700

    changed to using more managed app

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -2,11 +2,10 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+Bundler.require
+require "formula"
 
-module Formula
+module Dummy
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -30,13 +29,17 @@ module Formula
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
-
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
   end
 end
+
--- test/dummy/config/boot.rb
+++ test/dummy/config/boot.rb
@@ -1,13 +1,10 @@
 require 'rubygems'
+gemfile = File.expand_path('../../../../Gemfile', __FILE__)
 
-# Set up gems listed in the Gemfile.
-gemfile = File.expand_path('../../Gemfile', __FILE__)
-begin
+if File.exist?(gemfile)
   ENV['BUNDLE_GEMFILE'] = gemfile
   require 'bundler'
   Bundler.setup
-rescue Bundler::GemNotFound => e
-  STDERR.puts e.message
-  STDERR.puts "Try running `bundle install`."
-  exit!
-end if File.exist?(gemfile)
+end
+
+$:.unshift File.expand_path('../../../../lib', __FILE__)
