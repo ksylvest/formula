aa14345b71efe9cafb03e74de7cd66e874db7d72
Kevin Sylvestre <kevin@ksylvest.com>
2011-10-22 22:18:09 -0700

    switched to single config

---
+++ test/dummy/config/initializers/other.rb
@@ -0,0 +1,2 @@
+Dummy::Application.config.session_store :cookie_store, key: '_dummy_session'
+Dummy::Application.config.secret_token = 'fa14f99ebb5c5acadfb9c8fc423ff02fe8ed1b331916c29616471f5acfcb50c1227a1689ca390e03c77aa55b1826fd5b86c97773966a8925df32c6358db45a18'
--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Formula::Application.config.secret_token = 'fc3c38215bba19e62994650fc458087365078723bdd0970598429de0a684c729f97c0aa084e7f89fe1c71a5a0a6ccbf7a73910771cbe129e7cc491126d5e6db9'
