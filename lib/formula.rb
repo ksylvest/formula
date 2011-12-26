6e5859dd796426867c4ded8398721c9d8dece738
Iliya Grushevskiy <iliya.gr@gmail.com>
2011-12-26 22:27:35 +0300

    Added errors length check.

--- lib/formula.rb
+++ lib/formula.rb
@@ -353,7 +353,7 @@ module Formula
     
     def error(method)
       errors = @object.errors[method] if @object
-      errors.to_sentence if errors
+      errors.to_sentence if errors.any?
     end
     
     
